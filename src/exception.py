282f198f07c36fc81857cd40a98792e303cdb030
ansh26748ar <89684482+ansh26748ar@users.noreply.github.com>
2025-11-06 23:13:16 +0530

    EDA
    Signed-off-by: ansh26748ar <89684482+ansh26748ar@users.noreply.github.com>

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):  # type: ignore
     _,_,exc_tb=error_detail.exc_info()
@@ -13,14 +13,14 @@ class CustomException(Exception):
     def __init__(self,error_message, error_detail:sys): # type: ignore
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_detail)
-        
+         
     def __str__(self):
         return self.error_message
         
         
-# if __name__=="__main__":
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Division by zero")
-#         raise CustomException(e,sys)
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e,sys)
